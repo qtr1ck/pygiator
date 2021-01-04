a36d0fb7790926e05227aa2eedf20a59c0815de5
MartinWeigl <martinweigl48@gmail.com>
2021-01-04 12:53:22 +0100

    Added threshold

--- plot.py
+++ plot.py
@@ -5,7 +5,20 @@ import plotly.graph_objects as go
 import numpy as np
 
 
-def draw_plot(code_a, code_b):
+class sim_marker():
+    
+    def __init__(self, threshold = 0.8):
+        self.cmap = [0, "rgb(255, 255, 255)"],[threshold, "rgb(255, 255, 255)"], [threshold, "rgb(255, 0, 0)"], [1, "rgb(255, 0, 0)"]
+
+    def set_threshold(self, threshold):
+        self.cmap = [0, "rgb(255, 255, 255)"],[threshold, "rgb(255, 255, 255)"], [threshold, "rgb(255, 0, 0)"], [1, "rgb(255, 0, 0)"]
+
+    def get_map(self):
+        return self.cmap
+
+
+
+def draw_plot(code_a, code_b, filename_A = "Code A", filename_B = "Code B"):
     fig = go.Figure()
 
     data_a = code_a.get_ctg_array()
@@ -15,11 +28,11 @@ def draw_plot(code_a, code_b):
     labels_b = code_b.get_clnstr_array()
 
     trace_a = go.Heatmap(
-        z=data_a, text=labels_a, name="Code A", showscale=False, colorscale=get_cmap(),
+        z=data_a, text=labels_a, name=filename_A, showscale=False, colorscale=get_cmap(),
         hovertemplate='Row: %{y}<br>Column: %{x}<br>String: \'%{text}\'<extra></extra>')
 
     trace_b = go.Heatmap(
-        z=data_b, text=labels_b, name="Code B", showscale=False, colorscale=get_cmap(),
+        z=data_b, text=labels_b, name=filename_B, showscale=False, colorscale=get_cmap(),
         hovertemplate='Row: %{y}<br>Column: %{x}<br>String: \'%{text}\'<extra></extra>')
 
     fig = make_subplots(rows=1, cols=2)
@@ -29,13 +42,18 @@ def draw_plot(code_a, code_b):
 
     data_a_sim = code_a.get_sim_array()
 
-    trace_a_sim = go.Heatmap(z=data_a_sim, visible=False, showscale=False,hoverinfo='skip', colorscale='Bluered', opacity=0.8) 
+    s = sim_marker()
+
+    trace_a_sim = go.Heatmap(z=data_a_sim, name=filename_A, visible=False, showscale=False, colorscale=s.get_map(),
+                            opacity=0.8,
+                             hovertemplate='Similarity: %{z}\'<extra></extra>') 
     fig.append_trace(trace_a_sim, 1, 1)
 
     fig.update_yaxes(title_text="Row", autorange="reversed", row=1, col=1)
     fig.update_yaxes(title_text="Row", autorange="reversed", row=1, col=2)
     fig.update_xaxes(title_text="Column", row=1, col=1)
     fig.update_xaxes(title_text="Column", row=1, col=2)
+
 
     # Add dropdown to switch visibility
     fig.update_layout(
@@ -50,7 +68,7 @@ def draw_plot(code_a, code_b):
                         method="restyle"
                     ),
                     dict(
-                        args=[{'visible': [True, True, True]}],
+                        args=[{'visible': [False, True, True]}],
                         label="Show similarity",
                         method="restyle"
                     )
